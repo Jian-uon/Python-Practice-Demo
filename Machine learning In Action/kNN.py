710781e2c3d036ed08f5c66ec7e3657572e56a89
swjtu_hj@163.com <swjtu_hj@163.com>
2018-03-23 22:03:48 +0800

    K近邻算法

--- Machine learning In Action/kNN.py
+++ Machine learning In Action/kNN.py
@@ -1,4 +1,6 @@
+#coding:utf-8
 from numpy import * 
+from os import listdir
 import operator
 
 def createDataSet():
@@ -7,15 +9,101 @@ def createDataSet():
 	return group, labels
 
 def classify0(inX, dataSet, labels, k):
-	dataSetSize = dataSet.shape[0]
-	diffMat = tile(inX, (dataSetSize)) - dataSet
+	dataSetSize = dataSet.shape[0] #shape 查看矩阵或数组维数
+	diffMat = tile(inX, (dataSetSize, 1)) - dataSet #tile 对inX进行复制
 	sqDiffMat = diffMat**2
-	sqDistances = sqDiffMat.sum(axis=1)
+	sqDistances = sqDiffMat.sum(axis=1) #axis=0对每一列求和 axis=1对每一行求和
 	distances = sqDistances**0.5
-	sortedDistIndicies = distances.argsort()
+	sortedDistIndicies = distances.argsort() #argsort 返回数组值从小到大的索引值
 	classCount = {}
 	for i in range(k):
-		coteIlabel = labels[sortedDistIndicies[i]]
+		voteIlabel = labels[sortedDistIndicies[i]]
 		classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
-	sortedClassCount = sorted(classCount.iteritems(), key = operator.itemgetter(1), reverse=Ture)
+	#sort 是应用在 list 上的方法，sorted 可以对所有可迭代的对象进行排序操作。
+	sortedClassCount = sorted(classCount.iteritems(), key = operator.itemgetter(1), reverse=True)
 	return sortedClassCount[0][0]
+
+def file2matrix(filename):
+	fr = open(filename)
+	arrayOLines = fr.readlines()
+	numberOfLines = len(arrayOLines)
+	returnMat = zeros((numberOfLines, 3))
+	classLabelVector = []
+	index = 0
+	for line in arrayOLines:
+		line = line.strip()
+		listFromLine = line.split('\t')
+		returnMat[index, :] = listFromLine[0:3]  #a = [1,2,3] print a[:] -> [1,2,3]
+		classLabelVector.append(int(listFromLine[-1]))# 索引的-1表示最后一个元素
+		index +=1
+	return returnMat, classLabelVector
+
+def autoNorm(dataSet):
+	minVals = dataSet.min(0)  #min(axis = 0/1) 列/行的最小值
+	maxVals = dataSet.max(0)
+	ranges = maxVals - minVals
+	normDataSet = zeros(shape(dataSet))
+	m = dataSet.shape[0]
+	normDataSet = dataSet - tile(minVals, (m, 1))
+	normDataSet = normDataSet/tile(ranges, (m, 1))
+	return normDataSet, ranges, minVals
+
+def datingClassTest():
+	hoRatio =  0.10
+	datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')
+	normMat, ranges, minVals = autoNorm(datingDataMat)
+	m = normMat.shape[0]
+	numTestVecs = int(m*hoRatio)
+	errorCount = 0.0
+	for i in range(numTestVecs):
+		classifierResult = classify0(normMat[i, :], normMat[numTestVecs:m, :], datingLabels[numTestVecs:m], 3)
+		print 'the classifier came back with: %d, the real answer is: %d' %(classifierResult, datingLabels[i])
+		if classifierResult != datingLabels[i]:
+			errorCount += 1.0
+	print 'the total error rate is: %f' % (errorCount/float(numTestVecs))
+
+def classifyPerson():
+	resultList = ['not at all', 'in small doses', 'in large doses']
+	percentTats = float(raw_input("percentage of time spent playing video games?") )
+	ffMiles = float(raw_input("frequent flier miles earned per year?"))
+	iceCream = float(raw_input("liters of ice cream consumed per year?"))
+	datingDataMat, datingLabels =file2matrix('datingTestSet2.txt')
+	normMat, ranges, minVals = autoNorm(datingDataMat)
+	inArr = array([ffMiles, percentTats, iceCream])
+	classifierResult = classify0((inArr-minVals)/ranges, normMat, datingLabels, 3)
+	print "You will probably like this person: ", resultList[classifierResult - 1]
+
+def img2vector(filename):
+	returnVect = zeros((1, 1024))
+	fr = open(filename)
+	for i in range(32):
+		lineStr = fr.readline()
+		for j in range(32):
+			returnVect[0, 32*i+j] = int(lineStr[j])
+	return returnVect
+
+def handwritingClassTest():
+	hwLabels = []
+	trainingFileList = listdir('trainingDigits')
+	m = len(trainingFileList)
+	trainingMat = zeros((m, 1024))
+	for i in range(m):
+		fileNameStr = trainingFileList[i]
+		fileStr = fileNameStr.split('.')[0]
+		classNumStr = int(fileStr.split('_')[0])
+		hwLabels.append(classNumStr)
+		trainingMat[i, :] = img2vector('trainingDigits/%s' % fileNameStr)
+	testFileList = listdir('testDigits')
+	errorCount = 0.0
+	mTest = len(testFileList)
+	for i in range(mTest):
+		fileNameStr = testFileList[i]
+		fileStr = fileNameStr.split('.')[0]
+		classNumStr = int(fileStr.split('_')[0])
+		vectorUnderTest = img2vector('testDigits/%s' % fileNameStr)
+		classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3)
+		print "the classifier came back with: %d, the real answer is: %d" % (classifierResult, classNumStr)
+		if classifierResult != classNumStr:
+			errorCount += 1.0
+	print "\nthe total number of errors is: %d" %errorCount
+	print "\nthe total error rate is: %f" % (errorCount/float(mTest))
