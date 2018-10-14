34764805191bdfd9cc1b1025f9772acf3fe2cad1
usedrose <735342728@qq.com>
2018-10-14 23:49:10 +0800

    update

--- WordCloud/wordCloud_test.py
+++ WordCloud/wordCloud_test.py
@@ -1,7 +1,9 @@
 # coding: utf-8
-from wordcloud import WordCloud
+from wordcloud import WordCloud, ImageColorGenerator
+from PIL import Image
 import matplotlib.pyplot as plt
 import jieba 
+import numpy as np
 
 def run():
     f = open(u'words2.txt', 'r').read()
@@ -11,19 +13,23 @@ def run():
         if len(w) > 1:
             a.append(w)
     text = r' '.join(a)
-
+    
+    bg = np.array(Image.open('bg.jpg'))
     wordcloud = WordCloud(
             background_color = 'white',
-            width = 1500,
-            height = 960,
-            margin = 10,
+            #width = 1500,
+            #height = 960,
+            #margin = 10,
+            mask = bg,
             font_path='C:/Windows/Fonts/simkai.ttf',
             ).generate(text)
 
-    plt.imshow(wordcloud)
+    image_colors=ImageColorGenerator(bg)
+
+    plt.imshow(wordcloud.recolor(color_func=image_colors))
     plt.axis('off')
     plt.show()
-    wordcloud.to_file('words_result2.png')
+    wordcloud.to_file('words_result3.png')
     return
 
 if __name__ == '__main__':
